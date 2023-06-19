4b3112b51c49ae305d9da9a8bede75f8ef3defb4
laurabisong <laurettabisongobu@gmail.com>
2023-06-19 09:26:39 -0700

    puzzle file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+*main - A programme that prints a line with puts function
+*Return:0
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return(0);
+}
