f6df7fa17969657193cc036abead6b064d8f79da
laurabisong <laurettabisongobu@gmail.com>
2023-06-19 09:37:43 -0700

    4puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
-*main - A programme that prints a line with puts function
-*Return:0
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return(0);
+	puts("\"programming is like building a multilingual puzzel");
+	return (0);
 }
